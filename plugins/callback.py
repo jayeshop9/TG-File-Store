63b623f026f5a73f666a9a37ae04a8c77cec70df
jayeshop9 <98966562+jayeshop9@users.noreply.github.com>
2022-02-03 20:45:31 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -70,11 +70,11 @@ async def about_cb(c, m):
 
 👨‍💻 𝐃𝐞𝐯𝐞𝐥𝐨𝐩𝐞𝐫: {owner.mention(style='md')}
 
-📢 𝐂𝐡𝐚𝐧𝐧𝐞𝐥: [NS BOT UPDATES](https://t.me/Ns_bot_updates)
+📢 𝐂𝐡𝐚𝐧𝐧𝐞𝐥: [NS BOT UPDATES](https://t.me/AX_botz)
 
-👥 𝐆𝐫𝐨𝐮𝐩: [Ns BOT SUPPORT](https://t.me/Ns_Bot_supporters)
+👥 𝐆𝐫𝐨𝐮𝐩: [Ns BOT SUPPORT](https://t.me/astroflix)
 
-🌐𝐒𝐨𝐮𝐫𝐜𝐞 𝐂𝐨𝐝𝐞: [Press Me 🥰](https://github.com/Ns-Bots/TG-File-Store)
+🌐𝐒𝐨𝐮𝐫𝐜𝐞 𝐂𝐨𝐝𝐞: [THIS IS NOT OPEN SOURCE ](https://t.me/AX_botz)
 """
 
     # creating buttons
